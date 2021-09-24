65565ad7d49b0e14af2bb9dca24ad1e61349ceba
bingyang_hu <zoey4y@gmail.com>
2021-09-24 13:14:55 -0700

    view and templates

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import ArticlePost
 
 def article_list(request):
-    return HttpResponse("Hello World!")
+    articles = ArticlePost.objects.all()
+
+    context = {'articles' : articles}
+
+    return render(request,'article/list.html', context)
